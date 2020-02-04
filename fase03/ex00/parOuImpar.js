9743936fca3779e0ea3efc341a6d69bcdd5f2117
vhytoria <59872722+vhytoria@users.noreply.github.com>
2020-02-04 08:33:53 -0300

    marvin

---
+++ fase03/ex00/parOuImpar.js
@@ -0,0 +1,11 @@
+function parOuImpar(numero){
+    var num = numero;
+    var resto = num% 2;
+    
+    if (resto === 0){
+        return "par";
+    }else{
+        return "Impar"
+    }
+    }
+  
