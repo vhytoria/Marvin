8307c5de24f6151e323426de345b05b8d79acc18
vhytoria <59872722+vhytoria@users.noreply.github.com>
2020-02-04 10:46:36 -0300

    marvin

---
+++ fase03/ex02/pares.js
@@ -0,0 +1,9 @@
+function pares(num1, num2){
+    var numero = num1, num2
+    var total = numero %2;
+    if(total === 0 ){
+        return ("Os numeros são pares")
+    }else {
+        return("Os numeros não são pares")
+    }
+}
