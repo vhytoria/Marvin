1b906960c791d0e2c4cd816d08ee66911217fa59
vhytoria <59872722+vhytoria@users.noreply.github.com>
2020-02-11 08:58:00 -0300

    marvin

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,15 @@
+function ateZero(num1){
+    var n = 0;
+    if(num1 > 0){
+        while(n <= num1){
+            console.log(n);
+            n++;
+        }
+    }
+    if (num1<=0){
+        while(n>=num1){
+            console.log(num1);
+            num1++;
+        }
+    }
+}
