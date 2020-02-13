c1e56a594458ab3fd7834c3a756a58614ac98fa2
vhytoria <59872722+vhytoria@users.noreply.github.com>
2020-02-13 09:41:51 -0300

    marvin

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,16 +1,15 @@
-function por42 (num1, num2){
-    var res = null;
-    var cont = 0;
+function por42(num1, num2){
+    var result = null
+    var contador = 0
     while ( num1 <= num2){
-        if(num1 % 42 === 0 ){
-           cont = cont + 1;
+        if ( num1 % 42 === 0){
+            contador = contador + 1
+        }if (contador ===2){
+            return (num1)
         }
-    if (cont === 2) {
-        return(num1);
+        num1++
+    } if (!result){
+    console.log("Não encontrado")
+    return false
     }
-    num1++
-} if (!res){
-    console.log("não encontrado")
-    return false 
-    }
-}
+} 
