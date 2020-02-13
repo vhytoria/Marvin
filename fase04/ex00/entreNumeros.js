1c672420c9f78242529f65f0238cfff0de047294
vhytoria <59872722+vhytoria@users.noreply.github.com>
2020-02-13 09:44:03 -0300

    marvin

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,6 +1,16 @@
-function entreNumeros(min, max){
-    while (min<= max) {
-	 console.log (min, max);
-	min++;
-} 
+function fizzbuzz(num1, num2){
+    var c = num1
+    while (c <= num2){
+        if (c % 3 === 0 && c % 5 === 0){
+        console.log(c  + " FizzBuzz")
+    } else if ( c % 5 === 0){
+        console.log(c + " Buzz")
+    } else if (c % 3 === 0 ){
+        console.log(c + " Fizz")
+    } else {
+        console.log(c)
+    }
+    c++
+    }
+    
 }
