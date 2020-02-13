570faa79b46d8eb9af4abf385e083f92cebab9c3
vhytoria <59872722+vhytoria@users.noreply.github.com>
2020-02-13 10:08:56 -0300

    marvin

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -2,13 +2,13 @@ function fizzbuzz(num1,num2){
     var n = num1
     while(n <= num2){
           if(n % 3 === 0 && n % 5 === 0){
-     console.log(n + " FizzBuzz ")
+     console.log(n + "FizzBuzz")
     }
      else if(n % 5 === 0){
-        console.log(n + " Buzz ")
+        console.log(n + "Buzz")
     }
     else if (n % 3 === 0){
-        console.log(n + " Fizz ")
+        console.log(n + "Fizz")
     } else {
            console.log(n)
     }
